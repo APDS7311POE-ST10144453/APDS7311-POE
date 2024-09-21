3395d0ed68f6b9079270f580b58221a4b4fca061
ST10195824 <ST10195824@vcconnect.edu.za>
2024-09-21 10:57:06 +0200

    committing so we can move repo to org

---
+++ client/src/components/ErrorTextBox.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./SwiftCodeTextBox.css"; // Import the external CSS file
+
+// Define the props interface for the component
+interface ErrorTextBoxProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  error: boolean;
+}
+
+const ErrorTextBox: React.FC<ErrorTextBoxProps> = ({ value, onChange, error }) => {
+  return (
+    <input
+      type="text"
+      value={value}
+      onChange={onChange}
+      className={`error-textbox ${error ? "error" : ""}`} // Apply "error" class when error is true
+    />
+  );
+};
+
+export default ErrorTextBox;
