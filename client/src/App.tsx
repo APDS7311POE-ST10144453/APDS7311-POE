f7ae2f72165db6dc27fac46fb6a638cd9a82d221
Anneme <ST10144453@vcconnect.edu.za>
2024-09-21 17:32:38 +0200

    added employee login and transactions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,8 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import CustomerDashboard from "./pages/CustomerDashboard";
 import CustomerPaymentForm from "./pages/CustomerPaymentForm";
+import EmployeeLogin from "./pages/EmployeeLogin";
+import Transactions from "./pages/Transactions";
 
 function App() {
   const location = useLocation();
@@ -13,13 +15,15 @@ function App() {
   return (
     <>
       {/* Conditionally render Navbar */}
-      {location.pathname !== "/customer-dashboard" && location.pathname !== "/customer-payment-form" && <Navbar />}
+      {location.pathname !== "/customer-dashboard" && location.pathname !== "/customer-payment-form" &&  location.pathname !== "/transactions" &&<Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/customer-dashboard" element={<CustomerDashboard />} />
         <Route path="/customer-payment-form" element={<CustomerPaymentForm />} />
+        <Route path="/employee-login" element={<EmployeeLogin />} />
+        <Route path="/transactions" element={<Transactions />} />
       </Routes>
     </>
   );
