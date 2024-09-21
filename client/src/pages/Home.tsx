a7b4ad316a156e3c94cf969d6d44315c8099b18a
ST10195824 <ST10195824@vcconnect.edu.za>
2024-09-21 13:39:07 +0200

    dsd

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,7 @@
-import SwiftCodeTextBox from "../components/SwiftCodeTextBox";
-
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-
-      <SwiftCodeTextBox />
     </div>
   );
 }
