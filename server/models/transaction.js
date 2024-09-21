71eb5f082bf529d5135d90fae81d8ab61c9f99c2
ST10180919 <st10180919@vcconnect.edu.za>
2024-09-21 12:02:00 +0200

    Added transaction model and route

--- server/models/transaction.js
+++ server/models/transaction.js
@@ -1,6 +1,35 @@
 const mongoose = require("mongoose");
+const Decimal128 = mongoose.Types.Decimal128;
 
 const userschema = mongoose.Schema({
+  senderAccountNumber: {
+    type: String,
+    required: true,
+  },
+  RecipientName: {
+    type: String,
+    required: true,
+  },
+  RecipeintBank: {
+    type: String,
+    required: true,
+  },
+  RecipientAccountNumber: {
+    type: String,
+    required: true,
+  },
+  TransferAmount: {
+    type: Decimal128,
+    required: true,
+  },
+  Currency: {
+    type: String,
+    required: true,
+  },
+  SwiftCode: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Transaction", transactionschema);
