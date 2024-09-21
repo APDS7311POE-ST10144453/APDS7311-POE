480b4dc5c3475eaa48099076e1e896920bdff428
ST10180919 <st10180919@vcconnect.edu.za>
2024-09-21 15:12:53 +0200

    Updated encryption to be stronger

--- server/helpers/encryption.js
+++ server/helpers/encryption.js
@@ -1,25 +1,27 @@
 const crypto = require("crypto");
 
 function encrypt(text) {
-  const algorithm = "aes-256-cbc";
+  const algorithm = "aes-256-gcm";
   const salt = crypto.randomBytes(16).toString("hex");
   const key = crypto.scryptSync(process.env.ENCRYPTION_KEY, salt, 32);
   const iv = crypto.randomBytes(16);
   const cipher = crypto.createCipheriv(algorithm, key, iv);
   let encrypted = cipher.update(text, "utf8", "hex");
   encrypted += cipher.final("hex");
-  return salt + ":" + iv.toString("hex") + ":" + encrypted;
+  const authTag = cipher.getAuthTag().toString('hex'); // Get authentication tag
+  return salt + ":" + iv.toString("hex") + ":" + encrypted + ":" + authTag;
 }
 
 function decrypt(text) {
-  const algorithm = "aes-256-cbc";
-  const [salt, iv, encrypted] = text.split(":");
+  const algorithm = "aes-256-gcm";
+  const [salt, iv, encrypted, authTag] = text.split(":");
   const key = crypto.scryptSync(process.env.ENCRYPTION_KEY, salt, 32);
   const decipher = crypto.createDecipheriv(
     algorithm,
     key,
     Buffer.from(iv, "hex")
   );
+  decipher.setAuthTag(Buffer.from(authTag, 'hex')); // Set authentication tag
   let decrypted = decipher.update(encrypted, "hex", "utf8");
   decrypted += decipher.final("utf8");
   return decrypted;
