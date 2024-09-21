8707c6e10ca55cedd0654425dde9d04f38e9db2f
Anneme <ST10144453@vcconnect.edu.za>
2024-09-21 12:03:08 +0200

    more changes, landing allllmost done

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,12 @@
+import "../css/Home.css";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>
+        Unlock your full <span className="potential">POTENTIAL</span> <br />
+        with E-Z Banking.
+      </h1>
     </div>
   );
 }
