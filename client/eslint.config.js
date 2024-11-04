0de4203243fa4fa9c908219e030846a99969fba7
Nicholas Meyer <ST10204902@vcconnect.edu.za>
2024-11-04 08:00:02 +0200

    Another test

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -2,15 +2,11 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from '@typescript-eslint/eslint-plugin'
+import * as tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 import react from 'eslint-plugin-react'
 
-export default tseslint.config({
-  extends: [
-    js.configs.recommended,
-    ...tseslint.configs.strictTypeChecked,
-    ...tseslint.configs.stylisticTypeChecked,
-  ],
+export default [{
   files: ['**/*.{js,jsx,ts,tsx}'],
   ignores: [
     'dist/**',
@@ -25,17 +21,21 @@ export default tseslint.config({
       ...globals.browser,
       React: true,
     },
+    parser: tsParser,
     parserOptions: {
       project: ['./tsconfig.app.json', './tsconfig.node.json'],
       tsconfigRootDir: import.meta.dirname,
     },
   },
   plugins: {
+    '@typescript-eslint': tseslint,
     'react-hooks': reactHooks,
     'react-refresh': reactRefresh,
     react,
   },
   rules: {
+    ...js.configs.recommended.rules,
+    ...tseslint.configs.recommended.rules,
     ...reactHooks.configs.recommended.rules,
     ...react.configs.recommended.rules,
     ...react.configs['jsx-runtime'].rules,
@@ -55,4 +55,4 @@ export default tseslint.config({
       version: '18.3',
     },
   },
-})
+}]
